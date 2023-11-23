f5f9b929a1c9863e5b3719f23e1283b81fb1560a
ai-developer-kunda <148326228+ai-developer-kunda@users.noreply.github.com>
2023-11-23 15:16:01 +0800

    Add files via upload

--- chat-script-cdn.js
+++ chat-script-cdn.js
@@ -6,21 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var chatMessages = document.getElementById('chat-messages');
 
     var awaitingDateRange = false;
-    var awaitingInfo = {
-        startDate: false,
-        endDate: false,
-        numberOfGuests: false,
-        roomType: false,
-        specialRequirements: false
-    };
-
-    var bookingInfo = {
-        startDate: '',
-        endDate: '',
-        numberOfGuests: 0,
-        roomType: '',
-        specialRequirements: ''
-    };
 
     toggleButton.addEventListener('click', function() {
         chatContainer.classList.toggle('closed');
@@ -36,15 +21,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function addChatOptions() {
         var options = [
-            { id: 'check-availability', text: 'I want to check for available rooms' },
-            { id: 'make-booking', text: 'I want to make a booking' },
-            { id: 'recommend-place', text: 'Recommend place to stay' }
+            { id: 'check-availability', text: 'Check for available rooms' },
+            { id: 'make-booking', text: 'Make a booking' },
+            { id: 'recommend-place', text: 'Recommend a place to stay' }
         ];
 
         options.forEach(function(option) {
             var button = document.createElement('button');
             button.textContent = option.text;
-            button.classList.add('chat-option', 'animated-option');
+            button.classList.add('chat-option');
             button.id = option.id;
             button.addEventListener('click', function() {
                 handleOptionClick(option.id);
@@ -54,21 +39,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function handleOptionClick(optionId) {
-        if (optionId === 'check-availability') {
-            addBotMessage("Please select a start date and an end date (yyyy-mm-dd to yyyy-mm-dd):");
-            chatInput.setAttribute('placeholder', 'yyyy-mm-dd to yyyy-mm-dd');
-            awaitingDateRange = true;
-        } else if (optionId === 'recommend-place') {
-            fetchPropertyListings();
+        switch (optionId) {
+            case 'check-availability':
+                addBotMessage("Please select a start date and an end date (yyyy-mm-dd to yyyy-mm-dd):");
+                chatInput.setAttribute('placeholder', 'yyyy-mm-dd to yyyy-mm-dd');
+                awaitingDateRange = true;
+                break;
+            case 'recommend-place':
+                fetchPropertyListings();
+                break;
+            // Additional cases can be added here
         }
     }
 
     function fetchPropertyListings() {
-        fetch(`https://chatbot-api-ywul.onrender.com/listings/`, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+        fetch(`https://chatbot-api-ywul.onrender.com/listings/`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -90,10 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    sendButton.addEventListener('click', function() {
-        sendMessage();
-    });
-
+    sendButton.addEventListener('click', sendMessage);
     chatInput.addEventListener('keypress', function(e) {
         if (e.key === 'Enter') {
             sendMessage();
@@ -117,28 +99,15 @@ document.addEventListener('DOMContentLoaded', function () {
     function queryOpenAI(message) {
         addBotMessage("Processing your request...");
 
-        const openAIRequestData = {
-            messages: [
-                { role: "user", content: message }
-            ]
-        };
+        const openAIRequestData = { messages: [{ role: "user", content: message }] };
 
         fetch(`https://chatbot-api-ywul.onrender.com/query-openai/`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(openAIRequestData)
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            addBotMessage(data.choices[0].message.content.trim());
-        })
+        .then(response => response.json())
+        .then(data => addBotMessage(data.choices[0].message.content.trim()))
         .catch(error => {
             console.error('Error:', error);
             addBotMessage("Sorry, there was an error processing your request.");
@@ -149,7 +118,6 @@ document.addEventListener('DOMContentLoaded', function () {
         var dates = messageText.split(' to ');
         if (dates.length === 2 && isValidDate(dates[0]) && isValidDate(dates[1])) {
             checkRoomAvailabilityApi(dates[0], dates[1]);
-            awaitingDateRange = false;
         } else {
             addBotMessage("Please enter a valid date range (yyyy-mm-dd to yyyy-mm-dd):");
         }
@@ -157,29 +125,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function isValidDate(dateString) {
         var regex = /^\d{4}-\d{2}-\d{2}$/;
-        return dateString.match(regex) !== null;
+        return regex.test(dateString);
     }
 
     function checkRoomAvailabilityApi(startDate, endDate) {
         fetch(`https://chatbot-api-ywul.onrender.com/check-availability/?start_date=${startDate}&end_date=${endDate}`)
         .then(response => response.json())
-        .then(data => {
-            addBotMessage(data.message);
-            chatInput.setAttribute('placeholder', 'Type your message here...');
-            addChatOptions();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            addBotMessage("Sorry, there was an error checking availability.");
-        });
-    }
-
-    function processUserInput(messageText) {
-        // Process User Input (This function can be expanded based on specific needs)
-    }
-
-    function autoReply(messageText) {
-        // Auto-reply Functionality (Can be expanded or modified as per requirements)
+        .then(data => addBotMessage(data.message))
+        .catch(error => console.error('Error:', error));
     }
 
     function addBotMessage(message) {
